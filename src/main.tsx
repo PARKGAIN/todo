5cb945fdec273494af45248ac0b7faf461fde12e
parkgain <pgi082247@gmail.com>
2023-05-18 01:28:05 +0900

    chore : react-router-dom, axios 추가

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom/client";
 import App from "./App";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
